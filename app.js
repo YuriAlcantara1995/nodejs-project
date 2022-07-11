dead033b14fa13715b8d5cf38b587b4d91559727
Yuri Alcantara <yuri.alcantara1995@gmail.com>
2022-07-11 21:44:36 +0200

    adding cloud mongodb connection

--- app.js
+++ app.js
@@ -19,10 +19,14 @@ mongoose.connect(
   `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
   {
     useNewUrlParser: true,
-    useFindAndModify: false,
     useUnifiedTopology: true
   }
 );
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 //mongoose.connect("mongodb://localhost:27017/GithubGistDB");
 
